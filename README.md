cc2d624c6c6baaf95e2ecfc4a82b1ca17f3c31e6
Sergio <smrma1308@gmail.com>
2024-03-18 23:28:22 -0600

    modificar script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Tripleten web_project_around
+
+Descripción del proyecto.
+El proyecto web propuesto es una plataforma minimalista centrada en el registro y la compartición de fotografías de viajes. Los usuarios pueden crear una cuenta y luego subir imágenes de los lugares que han visitado. La característica principal de este sitio es su simplicidad. Su objetivo es permitir a los usuarios almacenar y mostrar sus experiencias de viaje a través de fotografías en un formato de galería, sin distracciones ni elementos de redes sociales. Cada usuario puede gestionar su propia galería de viajes, subiendo fotos a su perfil, sin más funcionalidades añadidas, manteniendo así el enfoque en las experiencias visuales y personales.
+
+Tecnologías utilizadas.
+
+- HTML
+- CSS
+- JavaScript
+- Grid
+- Flexbox
+- Git
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,10 @@
-const profileNameElement = document.querySelector(
-  ".profile__info--container-title"
-);
-const profileAboutElement = document.querySelector(".profile__info--subtitle");
+const profileNameElement = document.querySelector(".profile__name");
+const profileAboutElement = document.querySelector(".profile__about");
 
 const profileName = profileNameElement.textContent;
 const profileAbout = profileAboutElement.textContent;
 
-const profileEditButton = document.querySelector(
-  ".profile__info--container-button"
-);
+const profileEditButton = document.querySelector(".profile__edit-button");
 const editPopupElement = document.querySelector(".popup");
 
 const formElement = document.querySelector(".popup__form");
@@ -16,7 +12,7 @@ const nameInput = document.querySelector(".popup__input_name");
 const aboutInput = document.querySelector(".popup__input_about");
 const closeButtonPopup = document.querySelector(".popup__close-button");
 
-const profileAddButton = document.querySelector(".profile__add");
+const profileAddButton = document.querySelector(".profile__add-button");
 const addImagePopupElement = document.querySelector("#add-image-popup");
 const titleInput = document.querySelector(".popup__input_title");
 const imageInput = document.querySelector("#popup__input_image");
@@ -54,6 +50,7 @@ const initialCards = [
   },
 ];
 
+//Popup imagenes
 const popupImageOpen = document.querySelector("#popup_image-open");
 const closeImage = document.querySelector("#popup__close-image");
 const popupImageTitle = document.querySelector(".popup__image-title");
@@ -114,54 +111,54 @@ function handleAddImageFormSubmit(evt) {
   const newImageTitle = titleInput.value;
   const newImageUrl = imageInput.value;
   closeAddImagePopup();
+}
 
-  function cardGenerator(title, link) {
-    const card = templateCard.cloneNode(true).content.querySelector(".card");
-    const cardImage = card.querySelector(".card__photo");
-    const cardTitle = card.querySelector(".card__info-name");
-    const likeButton = card.querySelector(".card__like-button");
-    const deleteButton = card.querySelector(".card__delete-button");
-    cardImage.src = link;
-    cardTitle.textContent = title;
-    cardImage.alt = title;
-    likeButton.addEventListener("click", function () {
-      likeButton.classList.toggle("card__like-button_active");
-    });
-    deleteButton.addEventListener("click", function () {
-      card.remove();
-    });
-    cardImage.addEventListener("click", function () {
-      handleOpenImage(title, link);
-    });
-    return card;
-  }
-
-  initialCards.forEach(function (element) {
-    const newCard = cardGenerator(element.name, element.link);
-    cardArea.append(newCard);
+function cardGenerator(title, link) {
+  const card = templateCard.cloneNode(true).content.querySelector(".card");
+  const cardImage = card.querySelector(".card__photo");
+  const cardTitle = card.querySelector(".card__info-name");
+  const likeButton = card.querySelector(".card__like-button");
+  const deleteButton = card.querySelector(".card__delete-button");
+  cardImage.src = link;
+  cardTitle.textContent = title;
+  cardImage.alt = title;
+  likeButton.addEventListener("click", function () {
+    likeButton.classList.toggle("card__like-button_active");
   });
+  deleteButton.addEventListener("click", function () {
+    card.remove();
+  });
+  cardImage.addEventListener("click", function () {
+    handleOpenImage(title, link);
+  });
+  return card;
+}
 
-  function handleAddCardSubmit(evt) {
-    evt.preventDefault();
-    if (evt.submitter.classList.contains("popup__save-button")) {
-      const newCard = cardGenerator(titleInput.value, imageInput.value);
-      cardArea.prepend(newCard);
-    }
-    closeAddImagePopup();
-  }
+initialCards.forEach(function (element) {
+  const newCard = cardGenerator(element.name, element.link);
+  cardArea.append(newCard);
+});
 
-  function handleCloseImage() {
-    popupImageOpen.classList.remove("popup_opened");
-    overlayImage.removeEventListener("click", handleOverlayClick);
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  if (evt.submitter.classList.contains("popup__save-button")) {
+    const newCard = cardGenerator(titleInput.value, imageInput.value);
+    cardArea.prepend(newCard);
   }
+  closeAddImagePopup();
+}
 
-  function handleOpenImage(title, link) {
-    popupImage.src = link;
-    popupImageTitle.textContent = title;
-    popupImageOpen.classList.add("popup_opened");
-    closeImage.addEventListener("click", handleCloseImage);
-    overlayImage.addEventListener("click", handleOverlayClick);
-  }
+function handleCloseImage() {
+  popupImageOpen.classList.remove("popup_opened");
+  overlayImage.removeEventListener("click", handleOverlayClick);
+}
+
+function handleOpenImage(title, link) {
+  popupImage.src = link;
+  popupImageTitle.textContent = title;
+  popupImageOpen.classList.add("popup_opened");
+  closeImage.addEventListener("click", handleCloseImage);
+  overlayImage.addEventListener("click", handleOverlayClick);
 }
 
 function handleOverlayClick(event) {
